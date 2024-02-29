0765db552c9135f0a80949dfa0e047751bccbc10
Fiido <roccobosio@gmail.com>
2024-02-29 15:17:19 -0300

    add max height to namespace

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -11,21 +11,27 @@ export const Home = () => {
   return (
     <div className="h-[calc(100vh-68px)] flex items-center">
       <div className="text-white bg-primary p-4 w-1/2 m-auto rounded-md flex flex-col items-center justify-center">
-        <h2 className="text-4xl">¿Nuevo en Trello?</h2>
-        <h3 className="text-lg text-gray-200">
-          ¡Empieza ahora! <FontAwesomeIcon icon={faRocket} />
-        </h3>
         {!user.name ? (
-          <div className="mt-2 flex items-center justify-between w-full">
-            <Link to="/signup">
-              <Button>Registrarse</Button>
-            </Link>
-            <Link to="/signin">
-              <Button>Iniciar sesión</Button>
-            </Link>
-          </div>
+          <>
+            <h2 className="text-4xl">¿Nuevo en Trello?</h2>
+            <h3 className="text-lg text-gray-200">
+              ¡Empieza ahora! <FontAwesomeIcon icon={faRocket} />
+            </h3>
+            <div className="mt-2 flex items-center justify-between w-full">
+              <Link to="/signup">
+                <Button>Registrarse</Button>
+              </Link>
+              <Link to="/signin">
+                <Button>Iniciar sesión</Button>
+              </Link>
+            </div>
+          </>
         ) : (
-          <div className="mt-2">
+          <div className="mt-2 flex flex-col items-center justify-center">
+            <h2 className="text-4xl">¡Hola de nuevo!</h2>
+            <h3 className="text-lg text-gray-200">
+              ¡Tu dashboard te espera! <FontAwesomeIcon icon={faRocket} />
+            </h3>
             <Link to={"/dashboard"}>
               <Button>
                 Dashboard <FontAwesomeIcon icon={faDashboard} />
